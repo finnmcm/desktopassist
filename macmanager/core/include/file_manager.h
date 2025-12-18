f06aedaa28b102dcfcc95071f2aeb3f9a062715d
Finn McMillan <finnmcm@bu.edu>
2025-12-17 19:15:35 -0500

    condition var

--- macmanager/core/include/file_manager.h
+++ macmanager/core/include/file_manager.h
@@ -6,6 +6,14 @@
 #include <deque>
 #include <mutex>
 namespace macmanager {
+
+    struct DbWriteQueue {
+        std::mutex write_mutex;
+        std::deque<std::string> writeQueue;
+        std::atomic<int> workers_running{0};
+        std::condition_variable cv;
+    };
+
     class FileManager {
         public: 
         FileManager();
@@ -13,7 +21,8 @@ namespace macmanager {
         bool refresh_db_files(const std::vector<std::string>& locations, const std::set<std::string>& fileTypes, int numWorkers, Database& db);
 
         private:
-        void file_worker(int id, std::deque<fs::path>& workQueue, std::mutex& m, Database& db);
+        void file_worker(int id, std::deque<fs::path>& workQueue, std::mutex& work_mutex, Database& db, DbWriteQueue& dbWriteQueue);
+        void db_worker(int id, DbWriteQueue& dbWriteQueue, Database& db);
         std::filesystem::path root;
 
     };
--- macmanager/core/src/file_manager.cpp
+++ macmanager/core/src/file_manager.cpp
@@ -6,17 +6,21 @@
 
 namespace fs = std::filesystem;
 
+
 namespace macmanager {
     FileManager::FileManager(){
         root = std::getenv("HOME");
     }
-    bool FileManager::refresh_db_files(const std::vector<std::string>& locations, const std::set<std::string>& fileTypes, int numWorkers, Database& db){
+    bool refresh_db_files(const std::vector<std::string>& locations, const std::set<std::string>& fileTypes, int numWorkers, Database& db){
         std::vector<std::thread> threads;
+        //work queue for storing directories
         std::deque<fs::path> workQueue;
+        //depends on # root directories -> caller handled
+        DbWriteQueue dbWriteQueue;
         threads.reserve(numWorkers);
         std::mutex m;
         for(int i = 0; i < numWorkers; i++){
-            threads.emplace_back(file_worker, i, std::ref(workQueue), std::ref(m), std::ref(db));
+            threads.emplace_back(file_worker, i, std::ref(workQueue), std::ref(m), std::ref(dbWriteQueue), std::ref(db));
         }
         
         for(std::thread& t : threads){
@@ -46,7 +50,37 @@ namespace macmanager {
         fs::path documents = root / "Documents";
         return false;
     }
-    void file_worker(int id, std::deque<fs::path> workQueue, std::mutex& m, Database& db){
+    //WORKS FOR refresh_db_files
+    void file_worker(int id, std::deque<fs::path>& workQueue, std::mutex& work_mutex, DbWriteQueue& dbWriteQueue, Database& db){
+        for(;;){
+            //LOCK GUARD - define an inner scope, workQueue immediately unlocks when mutex goes out of scope 
+            //prevents early exits / exceptions crashing the program
+            fs::path curDir;
+            {
+                std::lock_guard<std::mutex> lock(m);
+                if(workQueue.empty()) return;
+                curDir = workQueue.front();
+                workQueue.pop_front();
+            }
+            std::error_code ec;
+            for(const auto& dirEntry : fs::directory_iterator(curDir, ec)){
+                if (ec) break;
+                fs::path fp = dirEntry.path();
+                if(dirEntry.is_directory() && !ec){
+                        std::lock_guard<std::mutex> lock(m);
+                        workQueue.push_back(fp);
+                }
+                else{
+                    const std::string filename = fp.stem().string();
+                    const std::string ext = fp.extension().string();
+                    //do something
+                }
+
+            }
+        }
+    }
+    void db_worker(int id, DbWriteQueue& dbWriteQueue, Database& db){
 
     }
+
 }
